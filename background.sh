50db52167f123740ea46193784e15af8de343cec
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-01 17:13:20 +0700

    add themes and icons

--- background.sh
+++ background.sh
@@ -3,3 +3,11 @@
 echo "🖼️ Background setup..."
 echo "Link:"
 echo "https://images.wallpapersden.com/image/download/simple-sunset-hd-digital_bG1paWeUmZqaraWkpJRobWllrWdma2U.jpg"
+
+echo "Themes setup..."
+echo "Link:"
+echo "https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1581082160/Orchis.tar.xz?response-content-disposition=attachment%3B%2520Orchis.tar.xz&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=RWJAQUNCHT7V2NCLZ2AL%2F20260601%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260601T101142Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=3199a261fe95f8c7d6a493334e74af5563c754089c8a4328619c0c17abb4533a"
+
+echo "Icons setup..."
+echo "Link:"
+echo "https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1544784225/01-Tela.tar.xz?response-content-disposition=attachment%3B%252001-Tela.tar.xz&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=RWJAQUNCHT7V2NCLZ2AL%2F20260601%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260601T083217Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=d347fb840550b1e966c49dfc8ad5e1929dc63b06c59c7f6391411e000ea269a9"
