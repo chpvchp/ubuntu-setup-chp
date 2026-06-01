13cec3efe0c7b26d90480f43e1acd180150d9bea
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-01 17:31:41 +0700

    update nvm and echo exec

--- dev.sh
+++ dev.sh
@@ -3,10 +3,10 @@
 echo "🧑‍💻 Dev environment..."
 
 # NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 
-export NVM_DIR="$HOME/.nvm"
-source "$NVM_DIR/nvm.sh"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 nvm install --lts
 
@@ -17,12 +17,12 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
 
-exec "$SHELL"
+echo "👉 Reload shell manually: exec SHELL"
 
 # Python 3.13.13
-sudo apt update; sudo apt install make build-essential libssl-dev zlib1g-dev \
+sudo apt update -y; sudo apt install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl git \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libzstd-dev
 
-pyenv install 3.13.13
+pyenv install -y 3.13.13
 pyenv global 3.13.13
